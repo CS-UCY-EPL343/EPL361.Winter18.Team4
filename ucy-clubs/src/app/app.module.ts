84f6e6cd35dd6e33b7e32c8a4a1928bf63152f4c
Andria <achrys16@cs.ucy.ac.cy>
2018-11-17 13:01:56 +0200

    creation of sing up

--- ucy-clubs/src/app/app.module.ts
+++ ucy-clubs/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CustomMaterialModule } from './material.module';
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -8,6 +9,7 @@ import { HomeComponent } from './home/home.component';
 import { VoteComponent } from './vote/vote.component';
 import { DanceClassesComponent } from './dance-classes/dance-classes.component';
 import { LogInComponent } from './log-in/log-in.component';
+import { NewComponentComponent } from './sing-up/new-component.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +17,15 @@ import { LogInComponent } from './log-in/log-in.component';
     HomeComponent,
     VoteComponent,
     DanceClassesComponent,
-    LogInComponent
+    LogInComponent,
+    NewComponentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    CustomMaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
