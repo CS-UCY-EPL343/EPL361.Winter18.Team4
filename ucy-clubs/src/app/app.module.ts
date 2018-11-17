f8f4cb33813e6bca859e7ff82c952ff5afb43d5c
Noni <aanaxa01@cs.ucy.ac.cy>
2018-11-17 17:33:29 +0200

    'A'

--- ucy-clubs/src/app/app.module.ts
+++ ucy-clubs/src/app/app.module.ts
@@ -7,18 +7,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { VoteComponent } from './vote/vote.component';
 import { DanceClassesComponent } from './dance-classes/dance-classes.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { NewsComponent } from './news/news.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
+// @ts-ignore
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     VoteComponent,
-    DanceClassesComponent
+    DanceClassesComponent,
+    ContactUsComponent,
+    AboutUsComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
