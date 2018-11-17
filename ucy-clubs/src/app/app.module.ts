4201d7b66454813205f144c86760e1fd6482aa12
smicha19 <20819972302pantera>
2018-11-17 17:03:47 +0200

    Creation Of a Dashboard

--- ucy-clubs/src/app/app.module.ts
+++ ucy-clubs/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatGridListModule} from '@angular/material/grid-list';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -8,6 +8,13 @@ import { HomeComponent } from './home/home.component';
 import { VoteComponent } from './vote/vote.component';
 import { DanceClassesComponent } from './dance-classes/dance-classes.component';
 import { UcyDashboardComponent } from './ucy-dashboard/ucy-dashboard.component';
+import {MatButtonModule} from "@angular/material";
+import {MatListModule} from "@angular/material";
+
+import { DanceClubComponent } from './dance-club/dance-club.component';
+
+
+
 
 @NgModule({
   declarations: [
@@ -15,12 +22,20 @@ import { UcyDashboardComponent } from './ucy-dashboard/ucy-dashboard.component';
     HomeComponent,
     VoteComponent,
     DanceClassesComponent,
-    UcyDashboardComponent
+    UcyDashboardComponent,
+    DanceClubComponent,
+
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatListModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
