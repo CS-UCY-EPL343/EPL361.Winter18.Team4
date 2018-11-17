f506f9ee9e712bb6eeaa514f75b0e7d39ab92db9
Andria <achrys16@cs.ucy.ac.cy>
2018-11-17 12:27:17 +0200

    creation of log in

--- ucy-clubs/src/app/app.module.ts
+++ ucy-clubs/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { VoteComponent } from './vote/vote.component';
 import { DanceClassesComponent } from './dance-classes/dance-classes.component';
+import { LogInComponent } from './log-in/log-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     VoteComponent,
-    DanceClassesComponent
+    DanceClassesComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
