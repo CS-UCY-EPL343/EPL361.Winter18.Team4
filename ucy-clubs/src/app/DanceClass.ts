f7e46cd7fc6f0d05c39855727974308703e112ce
stefiHB <skassi01@cs.ucy.ac.cy>
2018-11-17 11:44:39 +0200

    Merge branch 'master' of github.com:CS-UCY-EPL343/EPL361.Winter18.Team4
    Under contraction the Voting component and the mock-danceClass data
    
    # Conflicts:
    #	ucy-clubs/.editorconfig
    #	ucy-clubs/README.md
    #	ucy-clubs/angular.json
    #	ucy-clubs/e2e/src/app.e2e-spec.ts
    #	ucy-clubs/e2e/src/app.po.ts
    #	ucy-clubs/package-lock.json
    #	ucy-clubs/package.json
    #	ucy-clubs/src/app/app-routing.module.ts
    #	ucy-clubs/src/app/app.component.html
    #	ucy-clubs/src/app/app.component.spec.ts
    #	ucy-clubs/src/app/app.module.ts
    #	ucy-clubs/src/environments/environment.ts
    #	ucy-clubs/src/index.html
    #	ucy-clubs/src/main.ts
    #	ucy-clubs/src/polyfills.ts
    #	ucy-clubs/src/styles.css
    #	ucy-clubs/tsconfig.json
    #	ucy-clubs/tslint.json

---
+++ ucy-clubs/src/app/DanceClass.ts
@@ -0,0 +1,11 @@
+export class DanceClass {
+  id: number;
+  name: string;
+  // availDays: AvailableHours[];
+}
+
+// https://angular.io/tutorial/toh-pt1#two-way-binding
+class AvailableHours {
+  day: string;
+  hours: number[];
+}
--- ucy-clubs/src/app/app.module.ts
+++ ucy-clubs/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
+import { VoteComponent } from './vote/vote.component';
+import { DanceClassComponent } from './dance-class/dance-class.component';
+import { DanceClassesComponent } from './dance-classes/dance-classes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    VoteComponent,
+    DanceClassComponent,
+    DanceClassesComponent
   ],
   imports: [
     BrowserModule,
