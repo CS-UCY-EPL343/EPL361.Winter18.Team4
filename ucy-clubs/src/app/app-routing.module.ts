bfecb85eb37f1e3b291f6045d92433754eab06d8
Anastasios <ay222cn@lnu.se>
2018-11-23 01:14:47 +0200

    Routing

--- ucy-clubs/src/app/app-routing.module.ts
+++ ucy-clubs/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import { ShopComponent } from './UI-Shop/shop/shop.component';
+import { NewsComponent } from './news/news.component';
+import { SingupLoginComponent } from './singup-login/singup-login.component';
+import { ClubsComponent} from './clubs/clubs.component';
+import { DanceClubComponent } from './dance-club/dance-club.component';
+import { VoteComponent } from './vote/vote.component';
+import { AppComponent } from './app.component';
+
+
 
 const routes: Routes = [
+  {path: 'home', component: AppComponent},
+  {path: 'e-shop', component: ShopComponent},
+  {path: 'news', component: NewsComponent},
+  {path: 'clubs', component: ClubsComponent},
+  {path: 'sign-up-log-in', component: SingupLoginComponent},
+  {path: 'clubs/Dance-Club', component: DanceClubComponent,
+  children:[{path: 'Vote', component: VoteComponent}]},
+  {path: "*", redirectTo:'home'}
+  //{path: 'clubs/Dance-Club/Vote', component: VoteComponent, outlet: 'DanceClub'}
 
-  {path: 'e-shop', component: ShopComponent}, // OKAY
 ];
 
 
@@ -16,4 +32,4 @@ const routes: Routes = [
 
 })
 export class AppRoutingModule { }
-export const routingComponents = [ShopComponent]
+export const routingComponents = [ShopComponent, NewsComponent, ClubsComponent, SingupLoginComponent, DanceClubComponent, VoteComponent]
