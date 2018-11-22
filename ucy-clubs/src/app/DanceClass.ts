53118561b01d7b897daab4a1d1ad8d9cf7754da3
Andria <achrys16@cs.ucy.ac.cy>
2018-11-22 21:49:56 +0200

    Changes in mock dance classes

--- ucy-clubs/src/app/DanceClass.ts
+++ ucy-clubs/src/app/DanceClass.ts
@@ -10,11 +10,5 @@ export class AvailableHours {
   day: string;
   hours: number;
 
-  public toString = () :string=> {
-    let result: string;
-    result = this.day;
-    result = result.concat(" at " , String((this.hours + 9)));
-    result = result.concat(":00 - ",String((this.hours + 9)), ":59");
-    return result;
-  }
+
 }
